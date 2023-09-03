d58f96ace55989168c7129901c85e0bbf45663b0
houchuang <houchuang@outlook.com>
2023-09-03 20:03:37 +0800

    basic ok

--- src/main/resources/restart.sh
+++ src/main/resources/restart.sh
@@ -4,6 +4,7 @@ if [ -n "$pid"  ]; then
     kill -15 "$pid"
 fi
 
-script_path=$(cd $(dirname $0);pwd)
+# shellcheck disable=SC2046
+script_path=$(cd $(dirname $0) || exit;pwd)
 
 nohup java -cp "./bin/*:./lib/*" -Dstatic.resource="${script_path}/static/" -Dspring.profiles.active=prod com.bilanee.octopus.Application > console.log 2>&1 &
