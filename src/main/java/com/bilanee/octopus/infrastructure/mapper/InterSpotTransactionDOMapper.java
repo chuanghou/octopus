6d8617806c180fa5dd5f9768c8a81d51cd669d5f
houchuang <houchuang@outlook.com>
2023-10-14 10:28:20 +0800

    NPE-FIX, 调整创建接口，如果不传开始时间，默认无分钟之后开始

--- src/main/java/com/bilanee/octopus/infrastructure/mapper/InterSpotTransactionDOMapper.java
+++ src/main/java/com/bilanee/octopus/infrastructure/mapper/InterSpotTransactionDOMapper.java
@@ -1,7 +1,6 @@
 package com.bilanee.octopus.infrastructure.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.bilanee.octopus.infrastructure.entity.BidDO;
 import com.bilanee.octopus.infrastructure.entity.InterSpotTransactionDO;
 import org.apache.ibatis.annotations.Mapper;
 
