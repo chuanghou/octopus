e808211968c1d7604c09540e1e7b6d51ade898df
houchuang <houchuang@outlook.com>
2023-12-09 16:30:08 +0800

    add ssh command ability

--- src/main/java/com/bilanee/octopus/adapter/ws/WebSocket.java
+++ src/main/java/com/bilanee/octopus/adapter/ws/WebSocket.java
@@ -2,67 +2,85 @@ package com.bilanee.octopus.adapter.ws;
  
  
 import com.bilanee.octopus.basic.TokenUtils;
+import com.google.common.collect.Sets;
+import com.stellariver.milky.common.tool.common.Kit;
 import com.stellariver.milky.common.tool.util.Json;
+import lombok.CustomLog;
+import lombok.Data;
 import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import javax.websocket.OnClose;
-import javax.websocket.OnError;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
+
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.io.IOException;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 
 
-@Slf4j
+@Data
+@CustomLog
 @Component
 @ServerEndpoint("/ws")
 public class WebSocket {
 
-    @Getter
-    private Session session;
     private String userId;
+    private Session session;
 
-    private static final Map<String, Map<String, WebSocket>> sessions = new ConcurrentHashMap<>();
+    private static final Map<String, Set<Session>> sessions = new ConcurrentHashMap<>();
  
     @OnOpen
     public void OnOpen(Session session){
         this.session = session;
         this.userId = TokenUtils.getUserId(session.getRequestParameterMap().get("token").get(0));
-        String sessionId = session.getId();
-        sessions.computeIfAbsent(userId, k -> new ConcurrentHashMap<>()).put(session.getId(), this);
-        log.info("OnOpen sessions \n\n {}\n\n {} \n\n {}", Json.toJson(sessions.keySet()), this, session);
+        sessions.computeIfAbsent(this.userId, k -> Sets.newConcurrentHashSet()).add(this.session);
     }
 
     @OnClose
     public void OnClose(){
-        log.info("prepare close {}, {}, {}", userId, session, sessions);
-        Map<String, WebSocket> map = sessions.get(userId);
-        if (map == null) {
-            log.error(Objects.toString(this));
-            return;
-        }
-        WebSocket remove = map.remove(session.getId());
-        log.info("finish close \n\n {} \n\n {} \n\n {} \n\n {} \n\n {}", userId, session, remove, Json.toJson(sessions.keySet()), map.size());
+        log.info("close session, userId: {}, session : {}", userId, session);
+        Optional.ofNullable(sessions.get(userId)).ifPresent(ss -> ss.remove(session));
     }
 
     @OnError
     public void onError(Session session, Throwable error) {
-        log.error("sessions \n\n {} \n\n {}", sessions.size(), session, error);
+        log.error(" userId, {}, session: {}", userId, session, error);
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String message) {
+        Throwable backUp = null;
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (Throwable e) {
+            backUp = e;
+        }finally {
+            if (backUp == null) {
+                log.info("onMessage userId: {}, session : {}, wsMessage: {}", userId, session, message);
+            } else {
+                log.error("onMessage userId: {}, session : {}, wsMessage: {}", userId, session, message, backUp);
+            }
+        }
     }
 
     @SneakyThrows
     public static void cast(WsMessage wsMessage) {
-        List<Session> ss = sessions.values().stream().flatMap(m -> m.values().stream()).map(WebSocket::getSession).collect(Collectors.toList());
-        for (Session session : ss) {
-            session.getBasicRemote().sendText(Json.toJson(wsMessage));
-        }
+        sessions.values().stream().flatMap(Collection::stream).forEach(s -> {
+            Throwable backUp = null;
+            try {
+                s.getBasicRemote().sendText(Json.toJson(wsMessage));
+            } catch (Throwable e) {
+                backUp = e;
+            } finally {
+                if (backUp == null) {
+                    log.info("cast session : {}, wsMessage: {}", s, wsMessage);
+                } else {
+                    log.error("cast session : {}, wsMessage: {}", s, wsMessage, backUp);
+                }
+            }
+        });
     }
 
 }
