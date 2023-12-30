bfaf5eb8d1034376090c06e7800b69dc8c9abf48
houchuang <houchuang@outlook.com>
2023-12-31 00:24:36 +0800

    delete filter

---
+++ src/main/java/com/bilanee/octopus/infrastructure/handlers/ListQuestionHandler.java
@@ -0,0 +1,7 @@
+package com.bilanee.octopus.infrastructure.handlers;
+
+import com.bilanee.octopus.infrastructure.entity.Question;
+import com.stellariver.milky.infrastructure.base.database.ListJsonHandler;
+
+public class ListQuestionHandler extends ListJsonHandler<Question> {
+}
