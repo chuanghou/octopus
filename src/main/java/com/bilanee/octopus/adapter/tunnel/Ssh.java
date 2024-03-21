a925eee0d6971f6083d4130bb0806070ffb4dfc4
houchuang <houchuang@outlook.com>
2024-03-21 10:24:45 +0800

    change to local host

--- src/main/java/com/bilanee/octopus/adapter/tunnel/Ssh.java
+++ src/main/java/com/bilanee/octopus/adapter/tunnel/Ssh.java
@@ -24,7 +24,7 @@ public class Ssh {
         Session session = null;
         long s;
         try {
-            ssh.connect("118.184.179.113");
+            ssh.connect("localhost");
             ssh.authPassword("administrator", "gjdtsjtu@public0313");
             session = ssh.startSession();
             final Command cmd0 = session.exec("cd C:\\Users\\Administrator\\Desktop\\PowerMarketExperimentv2 & " + command);
