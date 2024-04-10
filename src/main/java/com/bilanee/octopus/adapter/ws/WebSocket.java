466ca95703411b3b7b6684b8b243845f025baca6
houchuang <houchuang@outlook.com>
2024-04-10 23:43:05 +0800

    config for windows

--- src/main/java/com/bilanee/octopus/adapter/ws/WebSocket.java
+++ src/main/java/com/bilanee/octopus/adapter/ws/WebSocket.java
@@ -2,7 +2,6 @@ package com.bilanee.octopus.adapter.ws;
 
 
 import com.bilanee.octopus.basic.TokenUtils;
-import com.google.common.collect.Sets;
 import com.stellariver.milky.common.tool.util.Json;
 import lombok.CustomLog;
 import lombok.Data;
@@ -11,11 +10,7 @@ import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
-import java.io.EOFException;
-import java.util.Collection;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -27,72 +22,54 @@ import java.util.concurrent.Executors;
 @ServerEndpoint("/ws")
 public class WebSocket {
 
-    private String userId;
-    private Session session;
     private static final Executor executor = Executors.newFixedThreadPool(100);
+    private static final Map<Session, String> sessions = new ConcurrentHashMap<>();
 
-    private static final Map<String, Set<Session>> sessions = new ConcurrentHashMap<>();
- 
     @OnOpen
     public void onOpen(Session session){
-        this.session = session;
-        this.userId = TokenUtils.getUserId(session.getRequestParameterMap().get("token").get(0));
-        sessions.computeIfAbsent(this.userId, k -> Sets.newConcurrentHashSet()).add(this.session);
+        String userId = TokenUtils.getUserId(session.getRequestParameterMap().get("token").get(0));
+        sessions.put(session, userId);
     }
 
     @OnClose
-    public void onClose(){
-        log.info("close session, userId: {}, session : {}", userId, session);
-        if (userId == null) {
-            return;
-        }
-        Optional.ofNullable(sessions.get(userId)).ifPresent(ss -> ss.remove(session));
+    public void onClose(Session session){
+        String userId = sessions.remove(session);
+        log.info("onClose userId: {}, session : {}", userId, session);
     }
 
     @OnError
     public void onError(Session session, Throwable error) {
-        if (error instanceof EOFException) {
-            log.warn(error.getMessage());
-        } else {
-            log.error(" userId, {}, session: {}", userId, session, error);
-        }
+        String userId = sessions.remove(session);
+        log.error("onError userId: {}, session : {}", userId, session, error);
     }
 
     @OnMessage
+    @SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
     public void onMessage(Session session, String message) {
-        Throwable backUp = null;
+        Throwable backUp;
+        String userId = sessions.get(session);
         try {
             synchronized (session) {
                 session.getBasicRemote().sendText(message);
             }
         } catch (Throwable e) {
-            backUp = e;
-        }finally {
-            if (backUp != null) {
-                log.error("onMessage userId: {}, session : {}, wsMessage: {}", userId, session, message, backUp);
-            }
+            log.error("onMessage userId: {}, session : {}, wsMessage: {}", userId, session, message, e);
         }
     }
 
     @SneakyThrows
     public static void cast(WsMessage wsMessage) {
-        sessions.values().stream().flatMap(Collection::stream).forEach(s -> {
-            executor.execute(() -> {
-                Throwable backUp = null;
-                try {
-                    synchronized (s) {
-                        s.getBasicRemote().sendText(Json.toJson(wsMessage));
-                    }
-                } catch (Throwable e) {
-                    backUp = e;
-                } finally {
-                    if (backUp != null) {
-                        log.error("cast session : {}, wsMessage: {}", s, wsMessage, backUp);
-                    }
+        sessions.keySet().forEach(session -> executor.execute(() -> {
+            String userId = sessions.get(session);
+            Throwable backUp = null;
+            try {
+                synchronized (session) {
+                    session.getBasicRemote().sendText(Json.toJson(wsMessage));
                 }
-            });
-
-        });
+            } catch (Throwable e) {
+                log.error("cast userId : {}, session : {}, wsMessage: {}", userId, session, wsMessage, e);
+            }
+        }));
     }
 
 }
